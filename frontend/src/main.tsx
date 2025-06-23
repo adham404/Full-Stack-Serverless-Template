1d23a94bdd3aaa33a6fe9ed3cc2264416b424663
adham404 <adham_mohsen@outlook.com>
2025-06-23 22:57:16 +0300

    refactor: third trial deployment of serverless application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,8 +8,6 @@ import { ApiError, OpenAPI } from "./client"
 import { CustomProvider } from "./components/ui/provider"
 
 
-
-
 OpenAPI.BASE = import.meta.env.VITE_API_URL
 OpenAPI.TOKEN = async () => {
   return localStorage.getItem("access_token") || ""
