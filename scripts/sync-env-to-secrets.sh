8de23ef72f3d3c80b72cbfa434d0d412dc3bb749
adham404 <adham_mohsen@outlook.com>
2025-06-24 01:15:48 +0300

    refactor:  trial number 17 deployment of serverless application

--- scripts/sync-env-to-secrets.sh
+++ scripts/sync-env-to-secrets.sh
@@ -9,7 +9,7 @@ echo "[INFO] Syncing secrets from $ENV_FILE to Secret Manager (project: $PROJECT
 
 while IFS='=' read -r key value; do
   if [[ "$key" =~ ^[A-Z_][A-Z0-9_]*$ && -n "$value" ]]; then
-    echo "[INFO] Processing secret: $key"
+    echo "[INFO] Processing secret: $key = $value"
 
     if gcloud secrets describe "$key" --project="$PROJECT_ID" >/dev/null 2>&1; then
       echo -n "$value" | gcloud secrets versions add "$key" --data-file=- --project="$PROJECT_ID"
@@ -26,17 +26,17 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
 CLOUDBUILD_SA="$PROJECT_NUMBER@cloudbuild.gserviceaccount.com"
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/secretmanager.secretAccessor"
+# gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+#   --member="serviceAccount:$CLOUDBUILD_SA" \
+#   --role="roles/secretmanager.secretAccessor"
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/firebasehosting.admin"
+# gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+#   --member="serviceAccount:$CLOUDBUILD_SA" \
+#   --role="roles/firebasehosting.admin"
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/serviceusage.serviceUsageConsumer"
+# gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+#   --member="serviceAccount:$CLOUDBUILD_SA" \
+#   --role="roles/serviceusage.serviceUsageConsumer"
 
 
 echo "[INFO] Done uploading secrets."
