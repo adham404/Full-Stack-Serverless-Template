7e0ab6170bac484be1c5bc1d643562c2820244b7
adham404 <adham_mohsen@outlook.com>
2025-06-24 22:06:04 +0300

    refactor: Serverless deployment trial number 43

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -9,8 +9,6 @@ from app.models import Item, ItemCreate, User, UserCreate, UserUpdate
 
 
 
-
-
 def create_user(*, session: Session, user_create: UserCreate) -> User:
     db_obj = User.model_validate(
         user_create, update={"hashed_password": get_password_hash(user_create.password)}
